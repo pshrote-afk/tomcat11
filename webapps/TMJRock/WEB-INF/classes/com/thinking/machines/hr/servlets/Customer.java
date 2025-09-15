9ace4bb88510363984b1caab0436d7b1e129ec2b
pshrote-afk <pshrote1@gmail.com>
2025-09-15 12:21:09 +0530

    add folder TMJRock
    +creating custom JavaScript library

---
+++ webapps/TMJRock/WEB-INF/classes/com/thinking/machines/hr/servlets/Customer.java
@@ -0,0 +1,15 @@
+package com.thinking.machines.hr.servlets;
+public class Customer
+{
+public String firstName;
+public String lastName;
+private int age;
+public void setAge(int age)
+{
+this.age = age;
+}
+public int getAge()
+{
+return this.age;
+}
+}
---
+++ webapps/TMJRock/WEB-INF/classes/com/thinking/machines/hr/servlets/ServletOne.java
@@ -0,0 +1,48 @@
+package com.thinking.machines.hr.servlets;
+import java.util.*;
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import com.thinking.machines.hr.dl.*;
+import com.google.gson.*;
+public class ServletOne extends HttpServlet
+{
+public void doPost(HttpServletRequest request,HttpServletResponse response)
+{
+try
+{
+response.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+}catch(Exception e)
+{
+//do nothing
+}
+}
+public void doGet(HttpServletRequest request,HttpServletResponse response)
+{
+try
+{
+PrintWriter pw = response.getWriter();
+response.setContentType("application/json");
+response.setCharacterEncoding("utf-8");
+DesignationDAO designationDAO = new DesignationDAO();
+List<DesignationDTO> designations = designationDAO.getAll();
+
+Gson gson = new Gson();
+JsonObject jsonObject = new JsonObject();
+jsonObject.addProperty("success",true);
+jsonObject.add("data",gson.toJsonTree(designations));
+String jsonString = gson.toJson(jsonObject);
+pw.print(jsonString);
+pw.flush();
+
+}catch(Exception e)
+{
+try
+{
+response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+}catch(Exception ee)
+{
+}
+}
+}//end doGet
+}//end class
