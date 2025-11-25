d07def992ff33f494199c9c92761ce4bdbfc382f
pshrote-afk <pshrote1@gmail.com>
2025-11-25 18:19:36 +0530

    stop tracking tmwebrock

--- webapps/tmwebrock/WEB-INF/classes/com/thinking/machines/webrock/annotations/Path.java
+++
@@ -1,9 +0,0 @@
-package com.thinking.machines.webrock.annotations;
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)		//default is RetentionPolicy.CLASS which is not available via java reflection
-@Target({ElementType.TYPE, ElementType.METHOD})
-public @interface Path
-{
-String value();
-}
